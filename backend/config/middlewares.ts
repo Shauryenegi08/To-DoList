2d73a73591fa511a4bf43b0991af32db521de324
Shauryenegi08 <negishaurye@gmail.com>
2025-07-11 12:11:54 +0530

    refactor middlewares configuration to streamline CORS setup and remove commented code

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,35 +1,3 @@
-// export default [
-//   'strapi::logger',
-//   'strapi::errors',
-//   'strapi::security',
-//   'strapi::cors',
-//   'strapi::poweredBy',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
-
-// module.exports = [
-//   'strapi::errors',
-//   {
-//     name: 'strapi::cors',
-//     config: {
-//       origin: ['http://localhost:3000'], // your frontend origin
-//       methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     },
-//   },
-//   'strapi::security',
-//   'strapi::poweredBy',
-//   'strapi::logger',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
-
 export default [
   'strapi::errors',
   {
